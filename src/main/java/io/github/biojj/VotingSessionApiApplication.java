3746ee9554f37c369cd88bba80456a5bb1666c30
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-25 21:39:54 -0400

    feat: ✨ init

---
+++ src/main/java/io/github/biojj/VotingSessionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.votingsessionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSessionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/biojj/VotingSessionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votingsessionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
